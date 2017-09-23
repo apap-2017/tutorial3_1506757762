94e351c8f88ef0d85e008956e639036085dcd408
ichsandyrizki <rizky.ichsandy@gmail.com>
2017-09-23 23:48:15 +0700

    Tutorial 3 Final

---
+++ src/main/java/com/example/tutorial3/service/InMemoryStudentService.java
@@ -0,0 +1,42 @@
+package com.example.tutorial3.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.example.tutorial3.model.StudentModel;
+
+public class InMemoryStudentService implements StudentService {
+	private static List<StudentModel> studentList = new ArrayList<StudentModel>();
+	
+	
+	
+	@Override
+	public StudentModel selectStudent(String npm) {
+		for(int i=0 ; i< studentList.size() ; i++) {
+			if(studentList.get(i).getNpm().equals(npm)) {
+				return studentList.get(i);
+			}
+		}
+		
+		return null;
+	}
+	
+	@Override
+	public List<StudentModel> selectAllStudents(){
+		return studentList;
+	}
+	 @Override
+	public void addStudent(StudentModel student) {
+		studentList.add(student);
+	}
+
+	@Override
+	public void removeStudent(String npm) {
+		// TODO Auto-generated method stub
+		for(int i=0 ; i< studentList.size() ; i++) {
+			if(studentList.get(i).getNpm().equals(npm)) {
+				studentList.remove(i);
+			}
+		}
+	}
+	
+}
